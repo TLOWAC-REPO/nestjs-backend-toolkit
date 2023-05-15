8be3c58ecd33a23a040c077ce51c9a02ddc1859f
ChangHun Lee <leechanghun55@gmail.com>
2023-05-15 20:35:17 +0900

    chore: prisma module 경로 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomConfigModule } from '@/provider';
+import { CustomConfigModule, PrismaModule } from '@/provider';
 
 
 @Module({
-        imports: [CustomConfigModule],
+        imports: [CustomConfigModule, PrismaModule],
         controllers: [AppController],
         providers: [AppService],
 })
