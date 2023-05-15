7d95de693066af34fed18941c59fd0339b399173
root <root@ChLEE>
2023-05-15 20:37:27 +0900

    feat(utils): swagger 설정 추가

---
+++ src/config/swagger.ts
@@ -0,0 +1,34 @@
+import { INestApplication, Logger } from "@nestjs/common";
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from "@nestjs/swagger";
+
+export class Swagger {
+  app: INestApplication;
+  document: OpenAPIObject;
+  logger: Logger = new Logger(Swagger.name);
+
+  constructor(app: INestApplication) {
+    this.app = app;
+    this.build();
+  }
+
+  private build() {
+    const options = new DocumentBuilder()
+      .setTitle("NESTJS Sample Project API")
+      .setDescription("This is a sample project")
+      .setVersion("1.0.0")
+      .addTag('Nestjs Swagger UI')
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'access-token',
+      )
+      .build();
+
+    const document = SwaggerModule.createDocument(this.app, options);
+    this.document = document;
+  }
+
+  pageSetup() {
+    SwaggerModule.setup("/docs", this.app, this.document);
+    this.logger.log("Swagger Setup");
+  }
+}
