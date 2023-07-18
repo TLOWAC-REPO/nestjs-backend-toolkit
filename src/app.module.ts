c22701ef5b8f5a6a031a91577a4d92786f9386fb
root <root@ChLEE>
2023-07-18 09:56:11 +0900

    chore: Logger 누락 코드 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomConfigModule, PrismaModule } from '@/provider';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from '@/common/interceptor';
 
-
 @Module({
         imports: [CustomConfigModule, PrismaModule],
         controllers: [AppController],
-        providers: [AppService,
+        providers: [
+                AppService,
                 {
                         provide: APP_INTERCEPTOR,
-                        useClass: LoggingInterceptor
-                }
+                        useClass: LoggingInterceptor,
+                },
+                Logger,
         ],
 })
 export class AppModule { }
